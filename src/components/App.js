a3369a56ee130d73aa8f4f942f0c7c4effb20917
milekag01 <milekag01@gmail.com>
2020-04-14 00:55:14 +0530

    update app component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,11 @@ import React from "react";
 
 class App extends React.Component {
     render() {
-        return <div>App Component</div>;
+        return (
+            <div>
+                <h1>App Component</h1>
+            </div>
+        );
     }
 }
 
